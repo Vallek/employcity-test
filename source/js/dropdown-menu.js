4fd2ec0123fa64ffd11c8ad4e590b7795ffd3736
Vallek <vforval86@gmail.com>
2025-06-26 15:13:24 +0300

    close menu on outside click

--- source/js/dropdown-menu.js
+++ source/js/dropdown-menu.js
@@ -1,5 +1,12 @@
+const menu = document.querySelector('.header__dropdown');
 const menuButton = document.querySelector('.dropdown__button');
 const menuDropdown = document.querySelector('.dropdown__nav');
+document.addEventListener('click', (evt) => {
+  let targetInside = menu.contains(evt.target);
+  if (!targetInside) {
+    menuDropdown.classList.add('hidden');
+  }	
+});
 menuButton.addEventListener('click', () => {
 	menuDropdown.classList.toggle('hidden');
 });
